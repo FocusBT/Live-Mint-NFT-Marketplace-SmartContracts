27d938ce7dd802957bcb4d4d9d08a7dd9d2df133
Wasi <k191357@nu.edu.pk>
2023-06-27 14:47:35 +0500

    ;;;;

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-verify");
-// require("dotenv").config({ path: ".env" });
+require("dotenv").config({ path: ".env" });
 // require("@nomicfoundation/hardhat-verify");
 const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
@@ -18,10 +18,8 @@ module.exports = {
   solidity: "0.8.7",
   networks: {
     sepholia: {
-      url: "https://bold-orbital-gas.ethereum-sepolia.discover.quiknode.pro/e8fce6401b498a84036e799cd15a645584802759/",
-      accounts: [
-        "8fe530fc5ea9fa6e49bb5b63ffbc5a979c7fda4de5992266804bc87c06478178",
-      ],
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
     },
   },
   contracts_build_directory: "./build",
