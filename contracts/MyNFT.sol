87bf2722f2bf939fde3ce4551c3529ca24dab3c2
Wasi <wasee62313@gmail.com>
2023-06-27 18:11:41 +0500

    Added new mech

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MyNFT is ERC721 {
+    using Strings for uint256;
+
+    string public _baseTokenURI;
+
+    // max number of LW3Punks
+    uint256 public maxTokenIds = 1;
+
+    // total number of tokenIds minted
+    uint256 public tokenIds;
+
+    /**
+     * @dev ERC721 constructor takes in a `name` and a `symbol` to the token collection.
+     * name in our case is `LW3Punks` and symbol is `LW3P`.
+     * Constructor for LW3P takes in the baseURI to set _baseTokenURI for the collection.
+     */
+    constructor(
+        string memory baseURI,
+        string memory tokenName,
+        string memory short
+    ) ERC721(tokenName, short) {
+        _baseTokenURI = baseURI;
+        mint();
+    }
+
+    function mint() public {
+        require(tokenIds <= maxTokenIds, "Exceed maximum token supply");
+        // require(msg.value >= _price, "Ether sent is not correct");
+        tokenIds += 1;
+        _safeMint(msg.sender, tokenIds);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory baseURI = _baseURI();
+        return
+            bytes(baseURI).length > 0
+                ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json"))
+                : "";
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomicfoundation/hardhat-verify");
+// require("@nomicfoundation/hardhat-verify");
 require("dotenv").config({ path: ".env" });
 // require("@nomicfoundation/hardhat-verify");
-const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const QUICKNODE_HTTP_URL = process.env.SEPHOLIA_RPC;
+const PRIVATE_KEY = process.env.METAMASK_PRIVATE_KEY;
 
 module.exports = {
   // paths: {
